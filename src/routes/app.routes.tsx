2ffb376185ee12cee06a23a09ec6b46001c5b11a
André Victor <andrevictor50@gmail.com>
2020-06-18 13:35:45 -0300

    🚀 added order details page

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -8,6 +8,7 @@ import TabRoutes from './tab.routes';
 
 import Home from '../pages/Home';
 import FoodDetails from '../pages/FoodDetails';
+import OrderDetails from '../pages/OrderDetails';
 
 const App = createStackNavigator();
 
@@ -33,6 +34,39 @@ const AppRoutes: React.FC = () => (
       <App.Screen
         name="FoodDetails"
         component={FoodDetails}
+        options={({ navigation }) => ({
+          headerLeft: () => (
+            <Icon
+              name="arrow-left"
+              size={24}
+              color="#FFB84D"
+              onPress={() => navigation.goBack()}
+            />
+          ),
+          headerLeftContainerStyle: {
+            marginLeft: 24,
+          },
+          headerRight: () => <Icon name="heart" size={24} color="#FFB84D" />,
+          headerRightContainerStyle: {
+            marginRight: 24,
+          },
+          headerTitle: 'Prato - Massas',
+          headerTitleStyle: {
+            color: '#fff',
+            fontFamily: 'Poppins-Regular',
+            fontSize: 16,
+          },
+          headerStyle: {
+            backgroundColor: '#C72828',
+            elevation: 0,
+            borderWidth: 0,
+            shadowColor: 'transparent',
+          },
+        })}
+      />
+      <App.Screen
+        name="OrderDetails"
+        component={OrderDetails}
         options={({ navigation }) => ({
           headerLeft: () => (
             <Icon
